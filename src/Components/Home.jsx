cda694d0743be0d0ddcdf9d827ca3471986266af
Shantanu <shantanusaini7000@gmail.com>
2024-03-03 13:47:49 +0530

    update home section

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import myImage from '../Images/my_image.png';
 import { FaLinkedinIn, FaInstagram, FaGithub } from "react-icons/fa";
 import { BsBoxArrowUpRight, BsBoxArrowInDown } from "react-icons/bs";
+import { SiGeeksforgeeks, SiLeetcode } from "react-icons/si";
 import myCV from '../Images/ShantanuCV.pdf';
 
 const Home = (props) => {
@@ -9,22 +10,32 @@ const Home = (props) => {
     return (
 
         <div id='home' className="flex md:flex-row flex-col md:justify-between items-center md:px-10  px-4 md:h-screen h-max" style={isLight ? { backgroundColor: "#FAF9F6" } : { backgroundColor: "#121212" }}>
-            <div className={`md:w-1/2 md:px-16 px-8 py-8 ${isLight?'text-black':'text-white'}`}>
-                <h1 className={`md:text-4xl text-2xl font-bold mb-4 ${isLight?'text-gray-800':'text-gray-200'}`}>Hi, I'm</h1>
+            <div className={`md:w-1/2 md:px-16 px-8 py-8 ${isLight ? 'text-black' : 'text-white'}`}>
+                <h1 className={`md:text-4xl text-2xl font-bold mb-4 ${isLight ? 'text-gray-800' : 'text-gray-200'}`}>Hi, I'm</h1>
                 <span className='text-red-700 md:text-6xl text-4xl font-bold italic'>Shantanu Saini</span>
                 <br /><br />
-                <h2 className="mb-6 md:text-5xl text-2xl">I'm a Programmer.</h2>
+                <h1 class={`animate-typing overflow-hidden whitespace-nowrap border-r-4 ${isLight ? 'border-r-gray-800' : 'border-r-gray-200'} md:text-5xl text-3xl ${isLight ? 'text-gray-800' : 'text-gray-200'} mb-5`}>I'm a programmer.</h1>
                 <div className='space-y-6 mb-10'>
                     <div className='flex space-x-6'>
-                        <div className={`h-12 w-12 flex items-center justify-center ${isLight?'bg-gray-200':'bg-gray-700'} rounded-full`}>
+                        <div className={`h-12 w-12 flex items-center justify-center ${isLight ? 'bg-gray-200' : 'bg-gray-700'} rounded-full`}>
                             <a href="https://www.linkedin.com/in/shantanu-saini-525a9a27a/" target="_blank" rel="noreferrer noopener"><FaLinkedinIn className='text-2xl' /></a>
                         </div>
-                        <div className={`h-12 w-12 flex items-center justify-center ${isLight?'bg-gray-200':'bg-gray-700'} rounded-full`}>
+                        <div className={`h-12 w-12 flex items-center justify-center ${isLight ? 'bg-gray-200' : 'bg-gray-700'} rounded-full`}>
                             <a href="https://www.instagram.com/whoshantanu_/?igshid=NTc4MTIwNjQ2YQ%3D%3D" target="_blank" rel="noreferrer"><FaInstagram className='text-2xl' /></a>
                         </div>
-                        <div className={`h-12 w-12 flex items-center justify-center ${isLight?'bg-gray-200':'bg-gray-700'} rounded-full`}>
+                        <div className={`h-12 w-12 flex items-center justify-center ${isLight ? 'bg-gray-200' : 'bg-gray-700'} rounded-full`}>
                             <a href="https://github.com/Shantanu-Saini" target="_blank" rel="noreferrer">
                                 <FaGithub className='text-2xl' />
+                            </a>
+                        </div>
+                        <div className={`h-12 w-12 flex items-center justify-center ${isLight ? 'bg-gray-200' : 'bg-gray-700'} rounded-full`}>
+                            <a href="https://auth.geeksforgeeks.org/user/whoshantanu/" target="_blank" rel="noreferrer">
+                                <SiGeeksforgeeks className="text-2xl" />
+                            </a>
+                        </div>
+                        <div className={`h-12 w-12 flex items-center justify-center ${isLight ? 'bg-gray-200' : 'bg-gray-700'} rounded-full`}>
+                            <a href="https://leetcode.com/whoshantanu/" target="_blank" rel="noreferrer">
+                                <SiLeetcode className='text-2xl' />
                             </a>
                         </div>
 
